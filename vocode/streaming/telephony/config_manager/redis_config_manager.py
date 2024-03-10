3f4645d28f9cbb570e545b0c2f963f964c28d32f
vishalai92 <159347998+vishalai92@users.noreply.github.com>
2024-03-10 15:40:15 -0700

    Updated TLS, SSL

--- vocode/streaming/telephony/config_manager/redis_config_manager.py
+++ vocode/streaming/telephony/config_manager/redis_config_manager.py
@@ -12,6 +12,7 @@ from vocode.streaming.telephony.config_manager.base_config_manager import (
 class RedisConfigManager(BaseConfigManager):
     def __init__(self, logger: Optional[logging.Logger] = None):
         use_tls = os.environ.get("REDIS_TLS", "false").lower() in ["true", "1", "t"]
+        ssl_cert_reqs = "none" if use_tls else None
         self.redis: Redis = Redis(
             host=os.environ.get("REDISHOST", "localhost"),
             port=int(os.environ.get("REDISPORT", 6379)),
@@ -19,6 +20,7 @@ class RedisConfigManager(BaseConfigManager):
             password=os.environ.get("REDISPASSWORD", None),
             db=0,
             ssl=use_tls,
+            ssl_cert_reqs=ssl_cert_reqs,
             decode_responses=True,
         )
         self.logger = logger or logging.getLogger(__name__)
