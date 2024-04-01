e677e37dd074c3e636f2852f1f34dd1461d9aa3f
vishalai92 <159347998+vishalai92@users.noreply.github.com>
2024-04-01 13:19:24 -0700

    Update endpointing to 0.5

--- vocode/streaming/models/transcriber.py
+++ vocode/streaming/models/transcriber.py
@@ -38,13 +38,13 @@ class EndpointingConfig(TypedModel, type=EndpointingType.BASE):
 
 
 class TimeEndpointingConfig(EndpointingConfig, type=EndpointingType.TIME_BASED):
-    time_cutoff_seconds: float = 0.4
+    time_cutoff_seconds: float = 0.5
 
 
 class PunctuationEndpointingConfig(
     EndpointingConfig, type=EndpointingType.PUNCTUATION_BASED
 ):
-    time_cutoff_seconds: float = 0.4
+    time_cutoff_seconds: float = 0.5
 
 
 class TranscriberConfig(TypedModel, type=TranscriberType.BASE.value):
